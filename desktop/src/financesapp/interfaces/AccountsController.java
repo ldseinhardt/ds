40618702e25a7695f544a39b4a8943c49f71db22
Rodrigo Oliveira <rodrigodroliveira@gmail.com>
2016-04-25 13:14:03 -0300

    Adicionando contas bancarias de forma dinamica (valeu, Luan!). Falta ainda uma scrollbar.

--- desktop/src/financesapp/interfaces/AccountsController.java
+++ desktop/src/financesapp/interfaces/AccountsController.java
@@ -4,19 +4,21 @@ import financesapp.Account;
 import financesapp.DefaultAccount;
 import financesapp.User;
 import java.net.URL;
+import java.text.NumberFormat;
 import java.util.Iterator;
-import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.AnchorPane;
 
 public class AccountsController implements Initializable {
     
     private User user;
     
     @FXML
-    private List<Label> labelList;
+    private AnchorPane accounts;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -30,12 +32,37 @@ public class AccountsController implements Initializable {
         user.addAccount(new DefaultAccount("Poupança" , 398.89));
 //------------
         Iterator<Account> ac = this.user.getAccounts().iterator();
-        Iterator<Label> lb = this.labelList.iterator();
+        int y = 0;
         while (ac.hasNext()) {
+            
             Account account = ac.next();
-            Label label = lb.next();
-            label.setText(account.getName());
-            label.setVisible(true);
+            
+            Label accName     = new Label(account.getName());
+            Label accBalance  = new Label(
+                                NumberFormat.
+                                getCurrencyInstance().
+                                format(account.getBalance()));
+            ProgressBar accPb = new ProgressBar(account.getBalance()/500);
+            //R$ 500,00: valor teste. Depois, 100% sera o maior saldo no mes.
+            
+            accounts.getChildren().add(accName);
+            accounts.getChildren().add(accBalance);
+            accounts.getChildren().add(accPb);
+            
+            accName   .setLayoutX (14);
+            accBalance.setLayoutX (200);
+            accPb     .setLayoutX (14);
+            accPb     .setPrefWidth(200);
+            
+            accName   .setLayoutY (44 + y);
+            accBalance.setLayoutY (44 + y);
+            accPb     .setLayoutY (61 + y);
+            
+            AnchorPane.setLeftAnchor  (accName   , 14.0);
+            AnchorPane.setRightAnchor (accBalance, 14.0);
+            AnchorPane.setLeftAnchor  (accPb     , 14.0);
+            AnchorPane.setRightAnchor (accPb     , 14.0);
+            y += 70;
         }
     }
     
