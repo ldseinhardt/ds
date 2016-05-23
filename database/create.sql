50b0c47c907f1e89c5da39544aa461c896f4fd62
Rodrigo Oliveira <Rodrigo Oliveira>
2016-05-23 10:30:52 -0300

    Correcoes profissionais do Luan no create.sql, adicionado insert.sql, test.sql (feitos pelo Luan) e oraculo.brM (Diagrama ER).

--- database/create.sql
+++ database/create.sql
@@ -1,66 +1,107 @@
 -- Script de criação do banco de dados
-CREATE DATABASE oraculo/*
-        DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci*/;
-        /*SQL_Latin1_General_CP1_CI_AI*/
+
+DROP DATABASE IF EXISTS oraculo;
+
+CREATE DATABASE oraculo;
+
 USE oraculo;
 
-CREATE TABLE Cidade (
-    idCidade         INT             NOT NULL AUTO_INCREMENT,
-    nomeCidade       VARCHAR(255)    NOT NULL,
-    PRIMARY KEY (idCidade)
-);
+-- Tabela de países
+CREATE TABLE IF NOT EXISTS `countries` (
+  `id`      INT         NOT NULL AUTO_INCREMENT,
+  `country` VARCHAR(64) NOT NULL UNIQUE,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE Estado (
-    idEstado         INT             NOT NULL AUTO_INCREMENT,
-    nomeEstado       VARCHAR(255)    NOT NULL,
-    PRIMARY KEY (idEstado)
-);
+-- Tabela de estados/regiões
+CREATE TABLE IF NOT EXISTS `states` (
+  `id`         INT         NOT NULL AUTO_INCREMENT,
+  `country_id` INT         NOT NULL,
+  `state`      VARCHAR(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE Pais (
-    idPais           INT             NOT NULL AUTO_INCREMENT,
-    nomePais         VARCHAR(255)    NOT NULL,
-    PRIMARY KEY (idPais)
-);
+-- Tabela de cidades
+CREATE TABLE IF NOT EXISTS `cities` (
+  `id`       INT         NOT NULL AUTO_INCREMENT,
+  `state_id` INT         NOT NULL,
+  `city`     VARCHAR(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`state_id`) REFERENCES `states` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE NivelAcesso (
-    nivel            SMALLINT        NOT NULL,
-    nomeNivel        VARCHAR(255),
-    PRIMARY KEY (nivel)
-);
+-- Tabela com o nome do tipo de usuários do sistema
+CREATE TABLE IF NOT EXISTS `levels` (
+  `id`    SMALLINT    NOT NULL AUTO_INCREMENT,
+  `level` VARCHAR(64) NOT NULL UNIQUE,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE Usuario (
-    email            VARCHAR(255)    NOT NULL,
-    senha            VARCHAR(255)    NOT NULL,
-    profissao        VARCHAR(255),
-    dataNasc         DATE,
-    nivel            SMALLINT        NOT NULL DEFAULT 1,
-    cidade           INT,
-    estado           INT,
-    pais             INT,
-    PRIMARY KEY (email),
-    FOREIGN KEY (nivel) REFERENCES NivelAcesso(nivel),
-    FOREIGN KEY (cidade) REFERENCES Cidade(idCidade),
-    FOREIGN KEY (estado) REFERENCES Estado(idEstado),
-    FOREIGN KEY (pais) REFERENCES Pais(idPais)
-) ENGINE = InnoDB;
+-- Tabela de profissões
+CREATE TABLE IF NOT EXISTS `occupations` (
+  `id`         INT         NOT NULL AUTO_INCREMENT,
+  `occupation` VARCHAR(64) NOT NULL UNIQUE,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE Categoria (
-    idCategoria      INT             NOT NULL AUTO_INCREMENT,
-    tipoCategoria    ENUM('Receita',
-	                      'Despesa') NOT NULL,
-    nomeCategoria    VARCHAR(255)    NOT NULL,
-    PRIMARY KEY (idCategoria)
-);
+-- Tabela com o nome de níveis de escolaridade
+CREATE TABLE IF NOT EXISTS `educations` (
+  `id`        INT         NOT NULL AUTO_INCREMENT,
+  `education` VARCHAR(64) NOT NULL UNIQUE,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE Transacao (
-    email            VARCHAR(255)    NOT NULL,
-    idTransacao      BIGINT          NOT NULL AUTO_INCREMENT,
-    dataTransacao    DATE            NOT NULL,
-    valor            DOUBLE(11,2)    NOT NULL,
-    categoria        INT             NOT NULL,
-    PRIMARY KEY (email, idTransacao),
-    FOREIGN KEY (email) REFERENCES Usuario(email)
-        ON DELETE CASCADE,
-    FOREIGN KEY (categoria) REFERENCES Categoria(idCategoria)
-        ON UPDATE CASCADE
-) ENGINE = MyISAM;
+-- Tabela de usuários
+CREATE TABLE IF NOT EXISTS `users` (
+  `email`                  VARCHAR(255) NOT NULL UNIQUE,
+  `name`                   VARCHAR(128) NOT NULL,
+  `password`               VARCHAR(64)  NOT NULL,
+  `city_id`                INT          NOT NULL,
+  `level_id`               SMALLINT     NOT NULL DEFAULT 1,
+  `active`                 TINYINT(1)   NOT NULL DEFAULT 1,
+  `password_token`         VARCHAR(64)  NULL,
+  `password_token_expires` TIMESTAMP    NULL,
+  `birthyear`              YEAR         NULL,
+  `occupation_id`          INT          NULL,
+  `education_id`           INT          NULL,
+  `created`                TIMESTAMP    NULL,
+  `modified`               TIMESTAMP    NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`email`),
+  FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`),
+  FOREIGN KEY (`level_id`) REFERENCES `levels` (`id`),
+  FOREIGN KEY (`occupation_id`) REFERENCES `occupations` (`id`),
+  FOREIGN KEY (`education_id`) REFERENCES `educations` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Tabela de arquivos
+CREATE TABLE IF NOT EXISTS `files` (
+  `file`       VARCHAR(255) NOT NULL UNIQUE,
+  `user_email` VARCHAR(255) NOT NULL,
+  `date`       DATE         NOT NULL,
+  PRIMARY KEY (`file`),
+  FOREIGN KEY (`user_email`) REFERENCES `users` (`email`)
+    ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Tabela de categorias
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id`       INT                        NOT NULL AUTO_INCREMENT,
+  `category` VARCHAR(64)                NOT NULL,
+  `type`     ENUM('Despesa', 'Receita') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Tabela de transações
+CREATE TABLE IF NOT EXISTS `transactions` (
+  `id`          INT          NOT NULL AUTO_INCREMENT,
+  `user_email`  VARCHAR(255) NOT NULL,
+  `category_id` INT          NOT NULL,
+  `date`        DATE         NOT NULL,
+  `value`       DOUBLE(11,2) NOT NULL,
+  PRIMARY KEY (`user_email`, `id`),
+  FOREIGN KEY (`user_email`) REFERENCES `users` (`email`)
+    ON DELETE CASCADE,
+  FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
+    ON UPDATE CASCADE
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
---
+++ database/test.sql
@@ -0,0 +1,37 @@
+-- Script para teste do banco de dados
+
+USE oraculo;
+
+SELECT
+  `users`.`name` AS `user`,
+  `transactions`.`id`,
+  `transactions`.`date`,
+  `categories`.`category`,
+  `categories`.`type`
+FROM
+  `transactions`
+  INNER JOIN `users`
+    ON (`transactions`.`user_email` = `users`.`email`)
+  INNER JOIN `categories`
+    ON (`transactions`.`category_id` = `categories`.`id`)
+;
+
+DELETE FROM
+  `transactions`
+WHERE
+  `user_email` = 'admin@oraculodamascada.com.br'
+;
+
+SELECT
+  `users`.`name` AS `user`,
+  `transactions`.`id`,
+  `transactions`.`date`,
+  `categories`.`category`,
+  `categories`.`type`
+FROM
+  `transactions`
+  INNER JOIN `users`
+    ON (`transactions`.`user_email` = `users`.`email`)
+  INNER JOIN `categories`
+    ON (`transactions`.`category_id` = `categories`.`id`)
+;
